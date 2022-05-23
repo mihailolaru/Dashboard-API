d4aee7482cd5c7012451945e6cfe8f5919091093
Mihail OLARU <mihai_olaru@ymail.com>
2022-05-23 18:55:18 +0300

    developing the controller.

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -1,6 +1,7 @@
-import { Router } from 'express'; 
+import { Router, Response } from 'express'; 
 import { LoggerService } from '../logger/logger.service';
 export { Router } from 'express';
+import { IControllerRout } from './route.interface'
 
 export abstract class BaseController{
 	private readonly _router: Router;
@@ -13,8 +14,28 @@ export abstract class BaseController{
 		return this._router;
 	}
 
-	protected bindRoutes(routes: any[]){
-		this.router.get('path', funct);
+	public send<T>(res: Response, code: number, message: T){
+		res.type('application/json');
+		return res.status(code).json(message);
+	}
+
+	public ok<T> (res: Response, message: T){
+		return this.send<T>(res, 200, message);
+	}
+
+	public create(res: Response){
+		return res.sendStatus(201);
+	}
+
+	//Can also integrate error sending here etc.
+
+	protected bindRoutes(routes: IControllerRout[]){
+		for(const route of routes){
+			this.logger.log(`[${route.method}] ${route.path}`);
+			//using bind to bind the context of the current class with the 'route.func' (express function's)
+			const handler = route.func.bind(this);
+			this.router[route.method](route.path, handler);
+		}
 
 	}
 } 
--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,6 +1,10 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 
-export interface IRoute {
+export interface IControllerRout {
 	path: string;
-	funct: ()
+	func: (req: Request, res: Response, next: NextFunction) => void;
+	// Correct syntax. Creating an interface. There will be a syntax check for the Router methods.
+	//method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' |'put'>
+	// Error prone syntax. Static definition of verbs.
+	method: 'get' | 'post' | 'delete' | 'patch' |'put';
 }
